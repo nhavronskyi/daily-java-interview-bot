283d6e206044e64b35ffef68ac9a2b7fb42858d3
Nazar Havronskyi <nhavronskyi@gmail.com>
2025-04-21 22:31:11 +0200

    init

--- src/main/java/com/nhavronskyi/dailyjavainterviewbot/props/GoogleProps.java
+++ src/main/java/com/nhavronskyi/dailyjavainterviewbot/props/GoogleProps.java
@@ -5,11 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "google")
 public record GoogleProps(
         String apiKey,
-        String refreshToken,
         UrlProps url
 ) {
     public record UrlProps(
-            String redirectUrl,
             String geminiUrl
     ) {
     }
